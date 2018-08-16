1a0b9149dc9a0782e1fc07069be2cdd85966410f
Nguoi La <khanh.hb111@gmail.com>
2018-08-17 01:48:18 +0700

    moedi

--- js-music/index.js
+++ js-music/index.js
@@ -2,7 +2,7 @@
 // https://codepad.co/snippet/aSiGmsBE
 
 // var audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/Lecrae_-_Anomaly_(Lyric_Video).mp3');
-var audio = new Audio('/home/hbkhanh/Downloads/BookBlock/fall_all_in_you.mp3');
+var audio = new Audio('fall_all_in_you.mp3');
 audio.volume = 0.1;
 audio.play();
 
