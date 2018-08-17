05856fe9dd2f3245077ca3c95400f0627564ff1e
Nguoi La <khanh.hb111@gmail.com>
2018-08-17 20:41:04 +0700

    added more

--- untranslatable-js/index.js
+++ untranslatable-js/index.js
@@ -1,10 +1,10 @@
 var words = [{
   id: 1,
-  word: 'Good Morning, Bi',
+  word: 'Good Morning',
   language: 'English',
   description: 'Khi người Anh chúc Linh Sang buổi sáng'
 }, {
-  word: 'Bom Dia, little Lily',
+  word: 'Bom Dia',
   language: 'Portuguese',
   description: 'A bitter-sweet melancholic yearning for something beautiful that is now gone.'
 }, {
@@ -12,11 +12,11 @@ var words = [{
   language: 'Ancient Greek',
   description: 'Often translated as ‘happiness’, it really means the deepest kind of fulfilment, often comprising a flourishing work and love life.'
 }, {
-  word: 'Te amo, Linh Sang',
+  word: 'Te amo',
   language: 'Spanish',
   description: 'A place where we feel safe, a ‘home’ from which we draw our strength and inspiration.'
 }, {
-  word: 'Ohayou, Bi',
+  word: 'Ohayou',
   language: 'Japanese',
   description: 'An acute sensitivity to the transience of lovely things; a melancholic awareness that everything nice will fade combined with a rich enjoyment of this short-lived beauty.'
 }, {
@@ -40,7 +40,7 @@ var words = [{
   language: 'Maldivian',
   description: 'A state of calm that Stoic philosophers aspired to. It’s a lack of agitation that comes from understanding the ways of the universe, accepting fate, knowing what one can control and focusing only on the things one can actually change.'
 }, {
-  word: 'Chúc Bi một buổi sáng ngọt ngào',
+  word: 'Buổi sáng ngọt ngào',
   language: 'Người Điện Bàn',
   description: 'The quality of being attractive because of being imperfect in some way. Instead of getting annoyed and upset by imperfections, wabi-sabi suggests that we should see the flaw itself as being part of what is charming.'
 }];
@@ -122,17 +122,17 @@ function loadCard() {
   cardDesc.textContent = words[i].description;
 }
 
-//Function to easily remove classes, it takes two parameters: 
+//Function to easily remove classes, it takes two parameters:
 //1. The element to have a class removed
 //2. The name of the class we want to remove from our element
 function removeClass(el, _class) {
-  //Check if the element exists, 
+  //Check if the element exists,
   //if it has a class,
   //& specifically if it has the class we want to remove
   if (el && el.className && el.className.indexOf(_class) >= 0) {
     //Find the class to be removed (including any white space around it) using a regex search pattern
     var pattern = new RegExp('\\s*' + _class + '\\s*');
-    //Replace that search with white space, therefore removing the class 
+    //Replace that search with white space, therefore removing the class
     el.className = el.className.replace(pattern, ' ');
   }
 }
