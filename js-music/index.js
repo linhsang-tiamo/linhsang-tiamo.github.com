5da21fa2058dc9d5743e2734a2f1f83d2a11b295
Nguoi La <khanh.hb111@gmail.com>
2018-10-01 23:41:15 +0700

    Added more

--- js-music/index.js
+++ js-music/index.js
@@ -3,7 +3,7 @@
 
 // var audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/Lecrae_-_Anomaly_(Lyric_Video).mp3');
 var audio = new Audio('fall_all_in_you.mp3');
-audio.volume = 0.1;
+audio.volume = 1;
 var playPromise = audio.play();
 if (playPromise !== undefined){
     playPromise.then(_ => {
